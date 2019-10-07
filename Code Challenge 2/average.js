d0a4de86113b61cac5524e08a727c4f09d4e761b
shahzebgit <51449693+shahzebgit@users.noreply.github.com>
2019-10-07 15:56:35 +0530

    Challenge 2

---
+++ Code Challenge 2/average.js
@@ -0,0 +1,13 @@
+var mikeTeam = (116+ 180 + 123)/ 3;
+var johnTeam = (116 + 180 + 123)/ 3;
+console.log(mikeTeam, johnTeam);
+
+if (mikeTeam > johnTeam) {
+    console.log('Mike\'s team has scored'+ mikeTeam +'than John\'s team'+ johnTeam);
+  
+} else if (johnTeam > mikeTeam) {
+    console.log('John\'s team has scored'+ johnTeam +'than Mike\'s team'+ mikeTeam);
+} else {
+    console.log('It\'s a DRAW!!!!');
+}
+    
