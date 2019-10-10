55f34fb2f6e97b4407ceddb2fe5fd28543b08fbe
shahzebgit <51449693+shahzebgit@users.noreply.github.com>
2019-10-10 10:20:42 +0530

    Add: changes

--- Code Challenge 1/calci.js
+++ Code Challenge 1/calci.js
@@ -1,11 +1,11 @@
 var markMass = 60; //in kg
-var markHeight= 1.8; // in meters
-var johnMass =70; //in kg
-var johnHeight =1.5;// in meters
-var mark1 = (markHeight*markHeight);
-var john1 = (johnHeight*johnHeight);
-var BMI1= markMass / mark1;
-var BMI2= johnMass / john1;
+var markHeight = 1.8; // in meters
+var johnMass = 70; //in kg
+var johnHeight = 1.5; // in meters
+var mark1 = (markHeight * markHeight);
+var john1 = (johnHeight * johnHeight);
+var BMI1 = markMass / mark1;
+var BMI2 = johnMass / john1;
 console.log(mark1, john1);
 console.log(BMI1, BMI2);
 
@@ -14,4 +14,3 @@ if (BMI1 > BMI2) {
 } else {
     console.log('John\'s BMI is Higher than Mark\'s');
 }
-
